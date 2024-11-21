52861de5dbb5978f310bf6791f5bb2bf428c554a
Azizbek <jonibekov97@gmail.com>
2024-11-21 15:59:31 +0500

    fix: change static assets folder

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Sky } from "three/addons/objects/Sky.js";
 import * as dat from "dat.gui";
 
 import "./style.css";
-
-function getImageUrl(name) {
-  return new URL(`./dir/${name}.png`, import.meta.url).href;
-}
 
 const gui = new dat.GUI();
 const textureLoader = new THREE.TextureLoader();
@@ -27,6 +24,57 @@ const setup = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+  const splashScreen = document.createElement("div");
+
+  splashScreen.style.position = "fixed";
+  splashScreen.style.width = "100%";
+  splashScreen.style.height = "100%";
+  splashScreen.style.background = "black";
+  splashScreen.style.display = "flex";
+  splashScreen.style.justifyContent = "center";
+  splashScreen.style.alignItems = "center";
+  splashScreen.style.color = "white";
+
+  THREE.DefaultLoadingManager.onStart = function (
+    url,
+    itemsLoaded,
+    itemsTotal
+  ) {
+    document.body.appendChild(splashScreen);
+    splashScreen.innerHTML = `Loading... ${(itemsLoaded / itemsTotal) * 100}%`;
+  };
+
+  THREE.DefaultLoadingManager.onLoad = function () {
+    console.log("Loading Complete!");
+    document.body.removeChild(splashScreen);
+  };
+
+  THREE.DefaultLoadingManager.onProgress = function (
+    url,
+    itemsLoaded,
+    itemsTotal
+  ) {
+    splashScreen.innerHTML = `Loading... ${
+      (Math.floor((itemsLoaded * 10) / itemsTotal) / 10) * 100
+    }%`;
+    console.log(
+      "Loading file: " +
+        url +
+        ".\nLoaded " +
+        itemsLoaded +
+        " of " +
+        itemsTotal +
+        " files."
+    );
+  };
+
+  THREE.DefaultLoadingManager.onError = function (url) {
+    console.log("There was an error loading " + url);
+  };
+
   return { scene, camera, renderer };
 };
 
@@ -36,11 +84,27 @@ const addLights = () => {
   const light = new THREE.AmbientLight("#86cdff", 0.275); // soft white light
   scene.add(light);
 
-  const directionalLight = new THREE.DirectionalLight("#86cdff", 1);
+  const directionalLight = new THREE.DirectionalLight("#86cdff", 1.5);
+  directionalLight.position.set(10, 10, -5);
+
+  directionalLight.castShadow = true;
+  directionalLight.shadow.mapSize.width = 1024;
+  directionalLight.shadow.mapSize.height = 1024;
+  directionalLight.shadow.camera.top = 8;
+  directionalLight.shadow.camera.right = 8;
+  directionalLight.shadow.camera.bottom = -8;
+  directionalLight.shadow.camera.left = -8;
+  directionalLight.shadow.camera.near = 1;
+  directionalLight.shadow.camera.far = 20;
+
   scene.add(directionalLight);
 
-  const pointLight = new THREE.PointLight("#ff7d46", 5);
-  pointLight.position.set(0, 2.2, 2.4);
+  const helper = new THREE.DirectionalLightHelper(directionalLight, 5);
+  scene.add(helper);
+
+  const pointLight = new THREE.PointLight("#ff7d46", 10);
+  pointLight.position.set(0, 2.2, 2.5);
+
   scene.add(pointLight);
 };
 
@@ -83,6 +147,9 @@ const addHouse = () => {
     })
   );
 
+  walls.castShadow = true;
+  walls.receiveShadow = true;
+
   /* Roof */
 
   const roofColorTexture = textureLoader.load(
@@ -121,24 +188,26 @@ const addHouse = () => {
   roof.position.y = 2.25;
   house.position.y = 2.5 / 2;
 
+  roof.castShadow = true;
+
   house.add(roof);
   house.add(walls);
   scene.add(house);
 };
 
 const addFloor = () => {
-  const alphaTexture = textureLoader.load("/haunted-house/floor/alpha.jpg");
+  const alphaTexture = textureLoader.load("/floor/alpha.jpg");
   const colorTexture = textureLoader.load(
-    "/haunted-house/floor/aerial_rocks_04_1k/aerial_rocks_04_diff_1k.jpg"
+    "/floor/aerial_rocks_04_1k/aerial_rocks_04_diff_1k.jpg"
   );
   const armTexture = textureLoader.load(
-    "/haunted-house/floor/aerial_rocks_04_1k/aerial_rocks_04_arm_1k.jpg"
+    "/floor/aerial_rocks_04_1k/aerial_rocks_04_arm_1k.jpg"
   );
   const normalTexture = textureLoader.load(
-    "/haunted-house/floor/aerial_rocks_04_1k/aerial_rocks_04_nor_gl_1k.png"
+    "/floor/aerial_rocks_04_1k/aerial_rocks_04_nor_gl_1k.png"
   );
   const displacementTexture = textureLoader.load(
-    "/haunted-house/floor/aerial_rocks_04_1k/aerial_rocks_04_disp_1k.jpg"
+    "/floor/aerial_rocks_04_1k/aerial_rocks_04_disp_1k.jpg"
   );
 
   colorTexture.repeat.set(4, 4);
@@ -173,6 +242,8 @@ const addFloor = () => {
     })
   );
 
+  floor.receiveShadow = true;
+
   gui
     .add(floor.material, "displacementScale")
     .min(0)
@@ -195,13 +266,13 @@ const addGraves = () => {
   const graves = new THREE.Group();
 
   const graveColorTexture = textureLoader.load(
-    "/haunted-house/tomb/plastered_stone_wall_1k/plastered_stone_wall_diff_1k.jpg"
+    "/tomb/plastered_stone_wall_1k/plastered_stone_wall_diff_1k.jpg"
   );
   const graveArmTexture = textureLoader.load(
-    "/haunted-house/tomb/plastered_stone_wall_1k/plastered_stone_wall_arm_1k.jpg"
+    "/tomb/plastered_stone_wall_1k/plastered_stone_wall_arm_1k.jpg"
   );
   const graveNormalTexture = textureLoader.load(
-    "/haunted-house/tomb/plastered_stone_wall_1k/plastered_stone_wall_nor_gl_1k.jpg"
+    "/tomb/plastered_stone_wall_1k/plastered_stone_wall_nor_gl_1k.jpg"
   );
 
   const roofRepeat = 1;
@@ -240,6 +311,9 @@ const addGraves = () => {
     grave.rotation.y = (Math.random() - 0.5) * 0.2;
     grave.rotation.z = (Math.random() - 0.5) * 0.2;
 
+    grave.castShadow = true;
+    grave.receiveShadow = true;
+
     graves.add(grave);
   }
 
@@ -247,21 +321,13 @@ const addGraves = () => {
 };
 
 const addDoor = () => {
-  const colorTexture = textureLoader.load("/haunted-house/door/color.jpg");
-  const aoTexture = textureLoader.load(
-    "/haunted-house/door/ambientOcclusion.jpg"
-  );
-  const alphaTexture = textureLoader.load("/haunted-house/door/alpha.jpg");
-  const normalTexture = textureLoader.load("/haunted-house/door/normal.jpg");
-  const displacementTexture = textureLoader.load(
-    "/haunted-house/door/height.jpg"
-  );
-  const metalnessTexture = textureLoader.load(
-    "/haunted-house/door/metalness.jpg"
-  );
-  const roughnessTexture = textureLoader.load(
-    "/haunted-house/door/roughness.jpg"
-  );
+  const colorTexture = textureLoader.load("/door/color.jpg");
+  const aoTexture = textureLoader.load("/door/ambientOcclusion.jpg");
+  const alphaTexture = textureLoader.load("/door/alpha.jpg");
+  const normalTexture = textureLoader.load("/door/normal.jpg");
+  const displacementTexture = textureLoader.load("/door/height.jpg");
+  const metalnessTexture = textureLoader.load("/door/metalness.jpg");
+  const roughnessTexture = textureLoader.load("/door/roughness.jpg");
 
   colorTexture.colorSpace = THREE.SRGBColorSpace;
 
@@ -287,11 +353,67 @@ const addDoor = () => {
   scene.add(door);
 };
 
+function initSky() {
+  // Add Sky
+  let sky = new Sky();
+  sky.scale.setScalar(450000);
+  scene.add(sky);
+
+  let sun = new THREE.Vector3();
+
+  /// GUI
+
+  const effectController = {
+    turbidity: 0.4,
+    rayleigh: 0,
+    mieCoefficient: 0.005,
+    mieDirectionalG: 0.735,
+    elevation: 23.2,
+    azimuth: 127.5,
+    exposure: renderer.toneMappingExposure,
+  };
+
+  function guiChanged() {
+    const uniforms = sky.material.uniforms;
+    uniforms["turbidity"].value = effectController.turbidity;
+    uniforms["rayleigh"].value = effectController.rayleigh;
+    uniforms["mieCoefficient"].value = effectController.mieCoefficient;
+    uniforms["mieDirectionalG"].value = effectController.mieDirectionalG;
+
+    const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
+    const theta = THREE.MathUtils.degToRad(effectController.azimuth);
+
+    sun.setFromSphericalCoords(1, phi, theta);
+
+    uniforms["sunPosition"].value.copy(sun);
+
+    renderer.toneMappingExposure = effectController.exposure;
+    renderer.render(scene, camera);
+  }
+
+  gui.add(effectController, "turbidity", 0.0, 20.0, 0.1).onChange(guiChanged);
+  gui.add(effectController, "rayleigh", 0.0, 4, 0.001).onChange(guiChanged);
+  gui
+    .add(effectController, "mieCoefficient", 0.0, 0.1, 0.001)
+    .onChange(guiChanged);
+  gui
+    .add(effectController, "mieDirectionalG", 0.0, 1, 0.001)
+    .onChange(guiChanged);
+  gui.add(effectController, "elevation", 0, 90, 0.1).onChange(guiChanged);
+  gui.add(effectController, "azimuth", -180, 180, 0.1).onChange(guiChanged);
+  gui.add(effectController, "exposure", 0, 1, 0.0001).onChange(guiChanged);
+
+  guiChanged();
+}
+
 addLights();
 addHouse();
 addGraves();
 addFloor();
 addDoor();
+initSky();
+
+scene.fog = new THREE.FogExp2("#5e6369", 0.05);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
